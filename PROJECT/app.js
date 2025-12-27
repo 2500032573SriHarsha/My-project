6931f4506cffb342451e2db06e2405b037bf5df5
2500032573SriHarsha <kolaventisriharsha@gmail.com>
2025-12-27 10:33:47 +0530

    Add files via upload

---
+++ PROJECT/app.js
@@ -0,0 +1,11 @@
+function saveData() {
+  alert("City information updated successfully!");
+}
+
+function reportIssue() {
+  alert("Issue reported. Authorities will take action.");
+}
+
+function sendFeedback() {
+  alert("Thank you for your feedback!");
+}
